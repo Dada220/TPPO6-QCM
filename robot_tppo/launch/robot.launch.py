905b8ea16f5dcca129121b8404508a0419b1282f
archie <nompos55@gmail.com>
2025-05-20 22:55:47 +0300

    Added camera marker detection. Added world and model for marker detection

--- robot_tppo/launch/robot.launch.py
+++ robot_tppo/launch/robot.launch.py
@@ -11,22 +11,21 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     pkg_share = get_package_share_directory('robot_tppo')
 
-    #world_name = 'empty.sdf'
-    #world_name = os.path.join(pkg_share,'worlds','green_cube.sdf')
-    world_name = os.path.join(pkg_share,'worlds','colorful_scene.sdf')
+    """
+    Worlds that are tested:
+    - aruco.sdf,
+    - colorful_scene.sdf
+    """
+    world_name = os.path.join(pkg_share,'worlds','aruco.sdf')
 
     xacro_file = os.path.join(pkg_share, 'urdf', 'robot.urdf.xacro')
     rviz_config_file = os.path.join(pkg_share, 'rviz', 'urdf.rviz')
     controller_params_file = os.path.join(pkg_share, 'config', 'diff_drive_controllers.yaml')
 
-    # 1) Генерируем Substitution для xacro -> urdf
     robot_description_substitution = Command(['xacro ', xacro_file])
-    # 2) Явно указываем, что это строка
     robot_description = ParameterValue(robot_description_substitution, value_type=str)
-    # 3) Готовим словарь
     robot_description_param = {'robot_description': robot_description}
 
-    # --- Узел robot_state_publisher ---
     rsp_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -35,20 +34,6 @@ def generate_launch_description():
         parameters=[robot_description_param]  # <-- передаём dict
     )
 
-    # --- Узел ROS2 Control (controller_manager) ---
-    # Removed because of dropped support for robot_description in ros2_control
-    # link: https://robotics.stackexchange.com/questions/114404/waiting-for-data-on-robot-description-topic-to-finish-initialization-i-tried
-    #ros2_control_node = Node(
-    #    package='controller_manager',
-    #    executable='ros2_control_node',
-    #    output='screen',
-    #    parameters=[
-    #        #           robot_description_param,  # словарь (robot_description)
-    #        controller_params_file    # путь к YAML
-    #    ]
-    #)
-
-    # Спавним контроллеры
     joint_state_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -69,15 +54,6 @@ def generate_launch_description():
         ],
         output='screen',
         parameters=[{'use_sim_time': True}]
-    )
-
-    # (Опционально) узел joint_state_publisher_gui
-    # Для дифф. робота обычно не нужен, но если хотите видеть слайдеры, оставьте
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        output='screen',
     )
 
     # --- RViz ---
@@ -101,7 +77,7 @@ def generate_launch_description():
             }.items()
     )
 
-    # --- Спавн робота в GZ Sim ---
+    # --- Robot spawn in GZ Sim ---
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
@@ -125,7 +101,6 @@ def generate_launch_description():
             "/camera/image@sensor_msgs/msg/Image@gz.msgs.Image",
             "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
             "/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan",
-
             "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
             "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
             "imu@sensor_msgs/msg/Imu@gz.msgs.IMU"
@@ -136,8 +111,6 @@ def generate_launch_description():
     # Собираем всё в LaunchDescription
     return LaunchDescription([        
         rsp_node,
-        #ros2_control_node,
-       #joint_state_publisher_node,
         rviz_node,
         gz_sim,
         gz_bridge,
--- robot_tppo/scripts/robot.py
+++ robot_tppo/scripts/robot.py
@@ -1,34 +1,29 @@
-"""
-robot class that allows for robot movement in 3D space
-"""
 import rclpy
-from rclpy.node import Node # for msg control
-
-from geometry_msgs.msg import TwistStamped
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Header
-
-from sensor_msgs.msg import Image,LaserScan,Imu
-from cv_bridge import CvBridge
-import cv2
-from rclpy.executors import MultiThreadedExecutor
 import threading
 import time
+import cv2
+from cv_bridge import CvBridge
+from rclpy.node import Node # for msg control
+from rclpy.executors import MultiThreadedExecutor
+from std_msgs.msg import Header
+from geometry_msgs.msg import TwistStamped,Twist
+from sensor_msgs.msg import Image,LaserScan,Imu
 
 class MessageBroker(Node):
     def __init__(self):
         super().__init__('img_tester')
-        self.init_sub_img()
-        self.init_pub_vel()
+        self._init_sub_img()
+        self._init_pub_vel()
 
-    def init_pub_vel(self):
+    def _init_pub_vel(self):
+        """Initialization of movement publisher"""
         self.pub_vel = self.create_publisher(
                 TwistStamped,
                 '/cmd_vel',
                 10
         )
-
-    def init_sub_img(self):
+    def _init_sub_img(self):
+        """Initialization of camera subscriber"""
         self.sub_img = self.create_subscription(
             Image,
             '/camera/image',
@@ -38,29 +33,41 @@ class MessageBroker(Node):
         self.bridge = CvBridge()
 
     def close_publishers(self):
+        """Closing all initialized publishers before closing MessageBroker"""
         self.destroy_publisher(self.pub_vel)
 
     def close_subscribers(self):
+        """Closing all initialized subscribers before closing MessageBroker"""
         self.destroy_subscription(self.sub_img)
 
     def close(self):
-        self.close_publishers()
-        self.close_subscribers()
-        self.destroy_node()
+        """Properly closing MessageBroker"""
+        self.close_publishers()     
+        self.close_subscribers()    
+        self.destroy_node()         
 
     def create_move_msg(self,direction):
+        """
+        Creates move TwistStamped message with set direction
+        Args:
+            direction(str): direction in which robot is moving
+        Returns:
+            msg(TwistStamped): movement message
+        """
         msg = TwistStamped()
+        # Because message is TwistStamped it consists of Header and Twist message
         msg.header = Header()
         msg.header.frame_id = "base_link" # Example frame ID
-        msg.twist = Twist()
 
+        msg.twist = Twist()
+        # Fixed robot directions
         directions = {
             'forward':  ((1.0,0.0,0.0),(0.0,0.0,0.0)),
             'backward': ((-1.0,0.0,0.0),(0.0,0.0,0.0)),
             'left':     ((0.0,0.0,0.0),(0.0,0.0,1.0)),
             'right':    ((0.0,0.0,0.0),(0.0,0.0,-1.0)),
         }
-
+        # Creating Twist part of the TwistStamped message
         try:
             vec_lin,vec_ang = directions[direction]
             msg.twist.linear.x = vec_lin[0]
@@ -71,22 +78,49 @@ class MessageBroker(Node):
             msg.twist.angular.z = vec_ang[2]
         except:
             print('Unknown direction')
+            msg.twist.linear.x = 0
+            msg.twist.linear.y = 0
+            msg.twist.linear.z = 0
+            msg.twist.angular.x = 0
+            msg.twist.angular.y = 0
         return msg
-
+    
     def publish_move_msg(self,direction):
+        """
+        Publishes move message with set direction
+        Args:
+            direction(str): direction in which robot is moving
+        """
         msg = self.create_move_msg(direction)
-        print(msg)
+        self.move_msg = msg
+        time.sleep(1)   # Adding delay for move command to finish its execution
         self.pub_vel.publish(msg)
-    # -------------------------------------------------------
+
     def camera_image_callback(self,msg):
+        """
+        Detects images in camera
+        Args:
+            msg: message from /camera/image topic 
+        """
+        # Converting image to black and white
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='mono8')
         except Exception as e:
             self.get_logger().error(f"Error converting image: {e}")
             return
 
-        #cv2.imshow('Camera Feed', cv_image)
-        #cv2.waitKey(1)  # Refresh the image
+        # WARNING: In Ubuntu cv2 version is 4.6.0 
+        # Same code but in 4.11.0 version:
+        #   aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_1000)
+        #   parameters = cv2.aruco.DetectorParameters()
+        #   detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+        #   corners, ids, rejected = detector.detectMarkers(image)
+
+        # Marker detection on image
+        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
+        parameters = cv2.aruco.DetectorParameters_create()
+        corners, ids, rejected = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
+        print("Detected markers:", ids)
 
 class Robot:
     def __init__(self):
@@ -94,21 +128,32 @@ class Robot:
         self._init_msg_broker()
 
     def _init_msg_broker(self):
+        """Initialization of message broker"""
         self.msg_broker = MessageBroker()
+        # Launching broker in separate thread to be able to use it 
         executor = MultiThreadedExecutor(num_threads=4)
         executor.add_node(self.msg_broker)
-    
         self.spin_thread = threading.Thread(target=executor.spin, daemon=True)
         self.spin_thread.start()
+
         while not rclpy.ok():
             time.sleep(0.5)
 
     def destroy(self):
-        self.msg_broker.close()
+        """Robot desctruction"""
+        self.msg_broker.close() 
         rclpy.shutdown()
         self.spin_thread.join() # proper thread cleanup before the program exits. 
 
-    def move_forward(self):  self.msg_broker.publish_move_msg('forward')
-    def move_backward(self): self.msg_broker.publish_move_msg('backward')
-    def turn_right(self):    self.msg_broker.publish_move_msg('left')
-    def turn_left(self):     self.msg_broker.publish_move_msg('right')
+    def move_forward(self): 
+        """Publishes a 'forward' movement command to the message broker."""
+        self.msg_broker.publish_move_msg('forward')
+    def move_backward(self): 
+        """Publishes a 'backward' movement command to the message broker."""
+        self.msg_broker.publish_move_msg('backward')
+    def turn_right(self):    
+        """Publishes a 'right' movement command to the message broker."""
+        self.msg_broker.publish_move_msg('right')
+    def turn_left(self):     
+        """Publishes a 'left' movement command to the message broker."""
+        self.msg_broker.publish_move_msg('left')
